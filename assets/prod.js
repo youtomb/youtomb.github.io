f097598ce7d1fb1781d4a77e6ea927def99faafd
youtomb <guh.huh.egg3@gmail.com>
2025-08-11 08:56:26 -0400

    ndd

--- assets/prod.js
+++ assets/prod.js
@@ -28406,14 +28406,14 @@
             a = new $(this.g,"actionTile","request-video-privacy","[[privacy ahh|Button that does what the title says.]]", "icon-youtube-logo", "[[this option tried to kill the debug button's liver|Description testing]]");
             this.b.push(a);    
             a = new $(this.g,"actionTile","request-loading-dialog","[[request loading dialog|Button that does what the title says.]]", "icon-desktop-esc", "[[this option tried to kill the sign in button, blame it for that.|Description testing]]");
-            this.b.push(a);    
-            if (this.h.cb) {
-                var a = new An(this.g,"toggleTile",this.W,"Show Video Info","")
+            this.b.push(a);  
+            if (this.h.cb = 0 || this.h.cb = 1) {
+                var a = new An(this.g,"toggleTile",this.W,"Show Video Info","[[Enable or disable video info.|Description for tile that toggles video info.]]")
                   , b = new $(this.g,"actionTile","request-debug-dialog","SHOW DEBUG INFO","warning");
                 this.b.push(a);
                 this.b.push(b)
             }
-            if (this.i.supportsSounds) {
+            if (this.i.supportsSounds = 1 || this.i.supportsSounds = 0) {
                 var a = new hp(this.h,"toggleTile",this.U.h,"[[Sounds|Title for a tile that toggles sounds.]]","[[Turn on or off application sounds.|Description for a tile that toggles sounds made by the YouTube TV application]]");
                 this.g.push(a)
             }
